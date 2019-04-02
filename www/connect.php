8b220d4db16407e52ff36dee34537b765811325c
leiperjack <leiperjack1997@gmail.com>
2019-04-02 14:00:44 +0100

    first commit

---
+++ www/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$username = "jack";
+$password = "root";
+$host = "db";
+$dbname = "football";
+
+$dbconnect = mysqli_connect($host, $username, $password, $dbname) or  die('Error: Could not connect (connect.php line 8)');
+?>
+
